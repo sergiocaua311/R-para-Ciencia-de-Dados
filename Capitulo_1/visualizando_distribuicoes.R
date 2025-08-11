4217504b46cf37bc35249295193192ec2d02dea6
Sérgio Cauã <sergio.caua.dos.santos@academico.ufpb.br>
2025-08-10 21:42:15 -0300

    Add script for visualizing distributions in R
    Introduces visualizations for categorical and numerical variables using the pinguins and diamante datasets. Includes bar plots, histograms, and density plots with examples of binwidth and bins parameters.

---
+++ Capitulo_1/visualizando_distribuicoes.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(dados)
+library(ggthemes)
+
+# Carregar os dados
+dados::pinguins
+
+# Variável Categorica 
+# Plota o gráfico sem estar ordenado
+ggplot(pinguins, aes(x = especie))+
+  geom_bar()
+
+#Plota o gráfico ordenado
+ggplot(pinguins, aes(x= fct_infreq(especie)))+
+  geom_bar()
+
+#O parâmetro binwith se refere ao intervalo no eixo X do histograma.
+ggplot(pinguins, aes(x = massa_corporal)) +
+  geom_histogram(binwidth = 250, na.rm = TRUE)
+
+#Vendo a função que estima a densidade:
+
+ggplot(pinguins, aes(x = massa_corporal)) +
+  geom_density(na.rm = TRUE)
+
+
+# Faça um gráfico de barras de especie de pinguins, no qual você atribui especie 
+# ao atributo estético y. 
+# Como esse gráfico é diferente?
+
+ggplot(pinguins, aes(y = especie)) +
+  geom_bar(na.rm = TRUE, color = "black",fill = "gray")
+
+# O que o argumento bins em geom_histogram() faz?
+
+ggplot(pinguins, aes(x = profundidade_bico)) + 
+  geom_histogram(bins = 50)
+
+# bins é a quantidade de intervalos que meu histograma terá.
+
+
+# Testando com outro conjunto de dados.
+
+dados::diamante
+
+
+ggplot(diamante, aes(x = quilate)) +
+  geom_histogram(na.rm = TRUE, binwidth = 0.1)
+
+
+
