315b0b844fbfe6035e30b261e56c24500e9c34f2
Sérgio Cauã <sergio.caua.dos.santos@academico.ufpb.br>
2025-08-14 15:06:58 -0300

    Create sintaxe_basica.R
    Teste de como as variáveis funcionam no R

---
+++ Capitulo_2/sintaxe_basica.R
@@ -0,0 +1,34 @@
+1/200 * 30
+
+# Atribuindo valores a variáveis
+x <- 1/200 * 30
+y <- 1/200 * 30
+
+x + y
+
+soma <- x + y
+
+#Exibindo o resultado
+soma
+
+#Vetores
+pares <- c(2,4,6,8)
+
+pares[5]
+
+#Multiplicação acontece elemento a elemento do vetor
+pares * 2
+
+# Atribuindo o resultado a própria variável
+pares <- pares * 3
+pares
+
+#Funções
+
+seq(from = 1, to = 10, by = 2)
+
+seq(1,10)
+
+#String 
+str  <- "Olá mundo!"
+str
