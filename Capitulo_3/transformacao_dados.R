9a521e3e1974f317d87ad429edea9f63cd1f7af9
Sérgio Cauã <sergio.caua.dos.santos@academico.ufpb.br>
2025-08-16 23:33:46 -0300

    Add data filtering and selection examples
    Introduced various examples of filtering and selecting data from the 'voos' dataset using dplyr functions, including usage of pipes, logical operators, and the %in% operator. Also added a placeholder for sorting functions.

--- Capitulo_3/transformacao_dados.R
+++ Capitulo_3/transformacao_dados.R
@@ -20,3 +20,29 @@ voos |>
   summarize(
     atraso_chegada = mean(atraso_chegada, na.rm = TRUE)
   )
+
+#Sem o pipe, esse parâmetro está em minutos de atraso
+filter(voos, atraso_saida > 100)
+
+#Com o pipe
+voos |>
+  filter(atraso_saida < 120)
+
+#Usando operadores & e |
+
+voos|>
+  filter(atraso_chegada > 5 & atraso_chegada < 10)
+
+#Outra forma de analisar e cpmbinar filtros é com %in%
+
+voos|>
+  filter(mes %in% c(2))
+
+fevereiro <- voos |>
+  filter(mes == 2)
+
+fevereiro
+
+#Funções de ordenação
+
+
