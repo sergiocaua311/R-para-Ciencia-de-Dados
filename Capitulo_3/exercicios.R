48da74312e9667302453a00a49ead05410dc4d07
Sérgio Cauã <sergio.caua.dos.santos@academico.ufpb.br>
2025-08-23 23:17:02 -0300

    Add data selection and column renaming examples
    Added examples demonstrating column selection with `select`, use of `any_of` and `contains`, and renaming a column in the `voos` tibble. These changes expand the set of data manipulation exercises in Capitulo_3/exercicios.R.

--- Capitulo_3/exercicios.R
+++ Capitulo_3/exercicios.R
@@ -6,3 +6,25 @@ dados::voos
 voos|>
   select(horario_saida,saida_programada, atraso_saida)|>
   view()
+
+voos|>
+  select(horario_saida, horario_saida, horario_chegada, horario_chegada)
+
+variables <- c("ano", "mes", "dia", "atraso_saida", "atraso_chegada","teste")
+
+voos|>
+  select(any_of(variables)) #Exibe as colunas que estão no tibble
+  
+voos |> select(contains("dia")) #Mostra as tabelas que tem essa sequencia de letras na sua composição
+#Não distinguindo MAIÚSCULAS e minúsculas 
+
+
+voos <-voos|>
+  rename("tempo_voo_min" = tempo_voo)
+
+colnames(voos)
+
+
+
+
+
